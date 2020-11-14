94334af52de7af977451b94b631cfb8e392a9529
unknown <unknown@example.com>
2020-11-14 08:39:18 +0800

    Initial Commit

---
+++ miniprogram/pages/mine/mine.js
@@ -0,0 +1,13 @@
+// pages/mine/mine.js
+Page({
+  data: {
+    userinfo:{}
+  },
+  onShow(){
+    const userinfo=wx.getStorageSync("userinfo");
+    const collect=wx.getStorageSync("collect")||[];
+      
+    this.setData({userinfo,collectNums:collect.length});
+      
+  }
+})
